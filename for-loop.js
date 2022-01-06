28e4d49565398622aacb830277b374ae900a63a5
Brenda <brendamvreal@gmail.com>
2022-01-05 21:09:04 -0500

    Initial commit with all my work so far

---
+++ for-loop.js
@@ -0,0 +1,12 @@
+let total = 0
+let limit = 10
+for (let i = 0; i < limit; i++){
+  total +=i;
+    
+} 
+console.log(total);
+
+// A) you assign some values to some variables
+// B) created a for loop that starts at 0 and exits once i goes over the variable limit
+// C) inside the for loop, add the value of i to total and at C, that value of i being added to total was run 10 times before the for loop terminated -ie. once the value of i exceeded limit
+// D) print out total
